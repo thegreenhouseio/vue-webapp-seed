83ceed75dd5925c0d0adf336f112e04704f86dbb
Owen Buckley <owen.buckley@analogstudios.net>
2017-09-10 11:22:32 -0400

    form handling

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -19,25 +19,71 @@
       </div>
     </div>
 
+    <div class="row">
+      <div class="col-xs-2">
+      </div>
+
+      <div class="col-xs-8">
+
+        <div class="posts-container">
+          <posts-list></posts-list>
+        </div>
+
+      </div>
+
+      <div class="col-xs-2">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-2">
+      </div>
+
+      <div class="col-xs-8">
+
+        <h2>Form Example</h2>
+        <p>Submit just outputs form (and values) to the console</p>
+
+        <form v-on:submit.prevent="onSubmit">
+
+          <label for="name">Name</label>
+          <input id="name" v-model="form.name" type="text" required/>
+
+          <br/>
+          <p v-if="form.name">Welcome to the site, {{form.name}}!</p>
+
+          <button v-on:click="submitForm" class="btn" type="submit">Submit</button>
+          <button class="btn" type="reset">Reset</button>
+
+        </form>
+
+      </div>
+
+      <div class="col-xs-2">
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-  //  export class HomeViewComponent {
-  //    private pristineName: string;
-  //    public userForm: FormGroup;
-  //
-  //    constructor(private FormBuilder: FormBuilder){
-  //    this.userForm = this.FormBuilder.group({
-  //      name: ''
-  //    })
-  //  }
-  //
-  //  submitForm(): void {
-  //    console.log('submit form');
-  //  console.log(this.userForm);
-  //  }
-  //  }
+  import PostsList from '../../components/posts-lists/PostsLists.vue';
+
+  export default {
+    data: function() {
+      return {
+        form: {
+          name: ''
+        }
+      };
+    },
+    components: { PostsList },
+    methods: {
+      submitForm: function() {
+        console.log('submit form', this.form); // eslint-disable-line
+      }
+    }
+  };
 </script>
 
 <style lang="scss">
